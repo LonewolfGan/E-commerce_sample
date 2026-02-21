80bd5655fba2a49c532870d875ef22e9ca70a192
LONEWOLF <131397613+LonewolfGan@users.noreply.github.com>
2026-02-21 22:41:35 +0000

    login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,6 +24,22 @@ export const Login = () => {
     }
   }
 
+  async function handleLoginGuest() {
+    email.current.value = "atlaslonewolf00@gmail.com";
+    password.current.value = "atlaslonewolf00@gmail.com";
+
+    try {
+      const authDetail = {
+        email: email.current.value,
+        password: password.current.value,
+      };
+      const data = await login(authDetail);
+      data.accessToken ? navigate("/products") : toast.error(data);
+    } catch (error) {
+      toast.error("Failed to fetch", { closeButton: true });
+    }
+  }
+
   return (
     <main>
       <section>
@@ -72,7 +88,10 @@ export const Login = () => {
         </button>
       </form>
       {
-        <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button
+          onClick={() => handleLoginGuest()}
+          className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
           Login As Guest
         </button>
       }
